74a7e516631875255a6992f374840c76207c1f2c
abgeranzt <abgeranzt@protonmail.com>
2020-07-12 18:43:21 +0200

    Slightly improved parser logging.

--- ts3bot/api/parser.py
+++ ts3bot/api/parser.py
@@ -11,11 +11,11 @@ class Parser:
 
     def parse_response(self, msg):
         """Parse response from interactive query and return list."""
-        self.logger.debug("Parsing response: (1/2)")
-        self.logger.debug(msg)
+        self.logger.debug("(1/2) Parsing response:")
+        self.logger.debug(f"(2/2) {msg}")
         msg_list = []
         # Remove leading "/r".
-        msg = re.split("\\r", msg)[1]
+        msg = re.split("\\r", msg, maxsplit=1)[1]
         msg = re.split("\|", msg)
         for entry in msg:
             entry = re.split(" ", entry)
@@ -28,8 +28,8 @@ class Parser:
 
     def parse_notify(self, msg):
         """Parse message from notifier query and return dict."""
-        self.logger.debug("Parsing notification: (1/2)")
-        self.logger.debug(msg)
+        self.logger.debug("(1/2) Parsing notification:")
+        self.logger.debug(f"(2/2) {msg}")
         msg_dict = {}
         msg_dict["KIND"], msg = re.split(" ", msg, maxsplit=1)
         msg_dict["CONTENT"] = []
