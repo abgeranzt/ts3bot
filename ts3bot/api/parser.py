64d5ae17200e68fc408acc58cb9f5afece195f95
abgeranzt <abgeranzt@protonmail.com>
2020-07-12 23:50:02 +0200

    Improved parsing.

--- ts3bot/api/parser.py
+++ ts3bot/api/parser.py
@@ -10,12 +10,18 @@ class Parser:
         self.logger = get_logger("parser")
 
     def parse_response(self, msg):
-        """Parse response from interactive query and return list."""
+        """
+        Parse response from interactive query.
+        - msg: str
+
+        Return list.
+        """
         self.logger.debug("(1/2) Parsing response:")
         self.logger.debug(f"(2/2) {msg}")
         msg_list = []
         # Remove leading "/r".
-        msg = re.split("\\r", msg, maxsplit=1)[1]
+        msg = re.sub("\\r", "", msg)
+        # Parse message.
         msg = re.split("\|", msg)
         for entry in msg:
             entry = re.split(" ", entry)
@@ -26,14 +32,25 @@ class Parser:
             msg_list.append(entry_dict)
         return msg_list
 
-    def parse_notify(self, msg):
-        """Parse message from notifier query and return dict."""
+    def parse_notify(self, msg, head):
+        """
+        Parse message from notifier query.
+        - msg: str
+        - head: str
+
+        Return dict.
+        """
         self.logger.debug("(1/2) Parsing notification:")
         self.logger.debug(f"(2/2) {msg}")
         msg_dict = {}
         # Remove leading "/r".
         msg = re.split("\\r", msg, maxsplit=1)[1]
         msg_dict["KIND"], msg = re.split(" ", msg, maxsplit=1)
+        # Split head from content.
+        head = re.search(head, msg).group(0)
+        msg_head, msg = re.split(head, msg)
+        msg_dict["HEAD"] = "".join([msg_head, head])
+        # Parse content.
         msg_dict["CONTENT"] = []
         msg = re.split("\|", msg)
         for entry in msg:
