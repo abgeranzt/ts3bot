54442c361dc34a5b7f1df73df4af1d9688b009f5
abgeranzt <abgeranzt@protonmail.com>
2020-07-14 18:52:55 +0200

    Parsing improvement.

--- ts3bot/api/parser.py
+++ ts3bot/api/parser.py
@@ -19,31 +19,48 @@ class Parser:
         self.logger.debug("Removing control characters.")
         return re.sub("\\n", "", re.sub("\\r", "", msg))
 
+    def parse_line(self, line):
+        """
+        Parse single message (body) line from query.
+        - msg: str
+        - ctrl_chars: bool
+
+        Return dict.
+        """
+        self.logger.debug("Parsing message line.")
+        line = self.remove_ctrl_chars(line)
+        line_dict = {}
+        line = re.split(" ", line)
+        for attr in line:
+            try:
+                key, val = re.split("=", attr, maxsplit=1)
+            except ValueError:
+                key, val = attr,""
+            # Convert string numbers to int.
+            if re.search("^\d+$", val):
+                val = int(val)
+            elif re.search("^\d+,", val):
+                val_list = []
+                for number in re.split(",", val):
+                    val_list.append(int(number))
+                val = val_list
+            line_dict[key.lower()] = val
+        return line_dict
+
     def parse_response(self, msg):
         """
-        Parse response from interactive query.
+        Parse response from query.
+        Wrapper for parse_line().
         - msg: str
 
-        Return list.
+        Return list with dicts.
         """
         self.logger.debug("Parsing response.")
-        msg_list = []
-        msg = self.remove_ctrl_chars(msg)
-        # Parse message.
+        msg_items = []
         msg = re.split("\|", msg)
-        for entry in msg:
-            entry = re.split(" ", entry)
-            entry_dict = {}
-            for attr in entry:
-                try:
-                    key, val = re.split("=", attr, maxsplit=1)
-                # Attribute has no value:
-                except ValueError:
-                    key = attr
-                    val = ""
-                entry_dict[key.upper()] = val
-            msg_list.append(entry_dict)
-        return msg_list
+        for line in msg:
+            msg_items.append(self.parse_line(line))
+        return msg_items
 
     def parse_notify(self, msg, head):
         """
