0f59da701a49ff5638209c54673d227a071dcdc9
abgeranzt <abgeranzt@protonmail.com>
2020-09-14 17:06:07 +0200

    refact: query interface

--- ts3bot/core/query/errors.py
+++ ts3bot/core/query/errors.py
@@ -2,6 +2,10 @@ class AuthError(ConnectionError):
     """Raise when query authentification fails."""
     pass
 
+class QueryError(Exception):
+    """Raise when query behaves unexpectedly."""
+    pass
+
 class QueryTimeout(Exception):
     """Raise when query did not produce output."""
     pass
